25a33b623bde3153a6514a2f8530aa5a3f6e40b0
htmltest <struev.vs@gmail.com>
2019-11-29 08:15:24 +0300

    menu+reference

--- js/tools.js
+++ js/tools.js
@@ -323,13 +323,24 @@ $(document).ready(function() {
     });
 
     $('.mobile-menu-link').click(function(e) {
-        $('html').toggleClass('mobile-menu-open');
+        if ($('html').hasClass('mobile-menu-open')) {
+            $('html').removeClass('mobile-menu-open');
+            $('meta[name="viewport"]').attr('content', 'width=device-width');
+        } else {
+            var curWidth = $(window).width();
+            if (curWidth < 480) {
+                curWidth = 480;
+            }
+            $('html').addClass('mobile-menu-open');
+            $('meta[name="viewport"]').attr('content', 'width=' + curWidth);
+        }
         e.preventDefault();
     });
 
     $(document).click(function(e) {
         if (!$(e.target).hasClass('mobile-menu-link') && $(e.target).parents().filter('.mobile-menu').length == 0) {
             $('html').removeClass('mobile-menu-open');
+            $('meta[name="viewport"]').attr('content', 'width=device-width');
         }
     });
 
